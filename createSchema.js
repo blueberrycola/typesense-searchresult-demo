b7b41334a916a4661115d37197e42d12914bcdf1
Chase Johnston <46426781+blueberrycola@users.noreply.github.com>
2021-08-08 23:42:59 -0400

    updated schema to fit 1cademy

--- createSchema.js
+++ createSchema.js
@@ -1,5 +1,6 @@
 const Typesense = require('typesense');
 
+const createdAt = 'Sun Aug 08 2021 23:32:23 GMT-0400 (Eastern Daylight Time)';
 
 let client = new Typesense.Client({
     'nodes': [{
@@ -14,20 +15,31 @@ let client = new Typesense.Client({
     When a field with facet is set to true it can be used as a
     toggle box to filter search results
 */
+
 let postSchema = {
     'name': 'posts',
     'fields': [
+        {'name': 'nodeID', 'type': 'string'},
         {'name': 'title', 'type': 'string'},
         {'name': 'author', 'type': 'string'},
-        {'name': 'postType', 'type': 'string', 'facet': true},
-        {'name': 'rating', 'type': 'int32'},
-
+        {'name': 'nodeType', 'type': 'string', 'facet': true}, //Categorical Attributes should be faceted if you want to cluster results
+        {'name': 'tags', 'type': 'string[]', 'facet': true},
+        {'name': 'proposers', 'type': 'string[]'},
+        {'name': 'upvotes', 'type': 'int32'},
+        {'name': 'rating', 'type': 'float'},
+        {'name': 'createdAt', 'type': 'string'},
+        {'name': 'changedAt', 'type': 'string'},
+        {'name': 'downvoted', 'type': 'int32'},
+        {'name': 'viewers', 'type': 'int32'},
+        {'name': 'bookmarks', 'type': 'int32'},
+        {'name': 'versionID', 'type': 'string[]'}
 
 
 
     ],
     'default_sorting_field': 'rating'
 };
+
 
 client.collections().create(postSchema)
     .then(function (data) {

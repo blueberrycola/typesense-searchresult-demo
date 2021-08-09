9b0573bc2415e923a816ae5786435774e3496315
Chase Johnston <46426781+blueberrycola@users.noreply.github.com>
2021-08-09 00:25:35 -0400

    added faker.js and updated uploadDocs()

--- seed.js
+++ seed.js
@@ -1,5 +1,5 @@
 const Typesense = require('typesense');
-
+const fake = require('faker');
 
 let client = new Typesense.Client({
     'nodes': [{
@@ -11,19 +11,63 @@ let client = new Typesense.Client({
     'connectionTimeoutSeconds': 2
 });
 
-var fname = ["Chase", "Shayla", "Drew", "Grant", "Ashlee", "Melissa", "Jeff", "Albert", "Larry", "Kanye", "Donda"];
-var lname = ["Johnston", "Fisher", "Smith", "Obama", "Biden", "Musk", "Johnson", "West", "Huestis", "East", "Einstein"];
 var title = ["That is just the luck of the Draw", "Piece of Cake", "Drive me Nuts", "A Lemon", "Close but No Cigar", "A Cold Day in Hell", "Everything but the Kitchen Sink", "Son of a Gun", "Fight Fire with Fire", "Beating a Dead Horse", "Rain on your Parade"];
 var postType = ["Data Science", "Mathematics", "Statistics", "Web Development", "Psychology", "Video Game Development", "Software Development"];
 var ratingCeiling = 10000;
 
-function createName() {
-    const randfName = Math.floor(Math.random() * fname.length);
-    const randlName = Math.floor(Math.random() * lname.length);
-    let str = fname[randfName];
-    str += ' ';
-    str += lname[randlName];
-    return str;
+//TODO: Rand gen for,  post-type,
+// tags[]
+//changedAt, createdAt, viewersINT, bookmarksINT
+
+//Used for nodeID, versionID
+function getRandomString(length) {
+    var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+    for ( var i = 0; i < length; i++ ) {
+        result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+    return result;
+}
+function getUser() {
+    return fake.internet.userName();
+}
+//Loads function for tags and postType
+//Bool is for if return should be str:str[]
+function majorArrayOrMajorString(flag) {
+    if(flag) {
+        return 'fooo';
+    } else {
+        return 'foo';
+    }
+    
+}
+
+function genUpDownVote(flag) {
+    if(flag) {
+        return Math.random() * 255;
+    } else {
+        return Math.random() * 128;
+    }
+}
+
+function genBookmarkViews(flag) {
+    if(!flag) {
+        return Math.random() * 1024;
+    } else {
+        return Math.random() * 64;
+    }
+}
+//Used for created and changedAt: CREATED MUST BE < CHANGED
+function randTimeStamps(flag) {
+    if(flag) {
+
+    } else {
+
+    }
+}
+
+function randUser() {
+
 }
 
 function createTitle() {
@@ -31,32 +75,40 @@ function createTitle() {
     return title[randTitle];
 }
 
-function createType() {
-    const randType = Math.floor(Math.random() * postType.length);
-    return postType[randType];
+//Gets random floating point for rating
+function getRandomFloat(){
+    return (Math.random() * (1.120 - 9.9999) + 0.0200).toFixed(4) * -1;
 }
+
 /* Uses the functions above to randomly generate document data for typesense */
 function uploadDocs() {
+    var foo;
+    for(var i = 0; i < 25; i++) {
 
-    for(var i = 0; i < 20; i++) {
-        var namestr = createName();
-        var titlestr = createTitle();
-        var createstr = createType();
-        var ratingnum = Math.floor(Math.random() * ratingCeiling);
-        
-        
         //Upload document to collection
         client.collections('posts').documents().create({
-            "title": titlestr,
-            "author": namestr,
-            "postType": createstr,
+            "nodeID": getRandomString(),
+            "title": createTitle(),
+            "author": getUser(),
+            "nodeType": createstr, //FIXME
+            "tags": foo, //FIXME
+            "proposers": foo, //FIXME
+            
+            "upvotes": genUpDownVote(true),
             "rating": ratingnum,
+            "createdAt": fake.date.past, 
+            "changedAt" : fake.date.future,
+
+            "downvoted": genUpDownVote(false),
+            "viewers": genBookmarkViews(true),
+            "bookmarks": genBookmarkViews(false),
+            "versionID": foo //FIXME genRandomString 0-15 times
+
 
         });
     }
     console.log("Documents Successfully Uploaded!")
 }
 
-uploadDocs();
 
 
